3037910a8de3ed0a7b3d98fa4ea2ab11b7d15040
Shea Lewis <shea.lewis89@gmail.com>
2015-02-04 15:29:26 -0800

    Laravel 5 support updates

--- src/Caffeinated/Flash/FlashServiceProvider.php
+++ src/Caffeinated/Flash/FlashServiceProvider.php
@@ -13,33 +13,25 @@ class FlashServiceProvider extends ServiceProvider
 	protected $defer = false;
 
 	/**
+	 * Boot the service provider
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->loadViewsFrom(__DIR__.'/../views', 'flash');
+
+		$this->publishes([
+			__DIR__.'/../views' => base_path('resources/views/vendor/flash'),
+		]);
+	}
+
+	/**
 	 * Register the service provider
 	 *
 	 * @return void
 	 */
 	public function register()
-	{
-		$this->registerServices();
-
-		$this->registerResources();
-	}
-
-	/**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides()
-	{
-		return ['flash'];
-	}
-
-	/**
-	 * Register the package services.
-	 *
-	 * @return void
-	 */
-	protected function registerServices()
 	{
 		$this->app->bind(
 			'Caffeinated\Flash\SessionStore',
@@ -52,13 +44,12 @@ class FlashServiceProvider extends ServiceProvider
 	}
 
 	/**
-	 * Register the package resources.
+	 * Get the services provided by the provider.
 	 *
-	 * @return void
+	 * @return array
 	 */
-	protected function registerResources()
+	public function provides()
 	{
-		// Add a view namespace
-		$this->app['view']->addNamespace('flash', __DIR__.'/../views');
+		return ['flash'];
 	}
 }
