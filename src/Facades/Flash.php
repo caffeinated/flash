f5801318b0a6cc294720c435184c6362bfc707d6
Michael Cordingley <Michael.Cordingley@gmail.com>
2017-07-20 12:51:56 -0400

    Document Methods Exposed by the Facade
    ...to help IDEs follow methods from the facade.

--- src/Facades/Flash.php
+++ src/Facades/Flash.php
@@ -3,6 +3,13 @@ namespace Caffeinated\Flash\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method static void error(string $message)
+ * @method static void info(string $message)
+ * @method static void message(string $message, string $level = 'info')
+ * @method static void success(string $message)
+ * @method static void warning(string $message)
+ */
 class Flash extends Facade
 {
 	/**
